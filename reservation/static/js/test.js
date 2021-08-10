81df1dd4c44cead6d4d124a46bab5bcdfdea7820
mathusankann <61835302+mathusankann@users.noreply.github.com>
2021-08-10 16:22:56 +0200

    -bugfix

--- reservation/static/js/test.js
+++ reservation/static/js/test.js
@@ -6,37 +6,40 @@ let index
 window.addEventListener('DOMContentLoaded', function () { //todo add getter with settings
     console.log(sessionStorage.getItem("BBB_meetingID"))
     setTimeout(async function () {
-        settings = await getter("https://reservation.jitsi-mathu.de/getKonfSettings")
-        getter("https://reservation.jitsi-mathu.de/getActiveMeetings?meetingID="+sessionStorage.getItem("BBB_meetingID")+"&name="+sessionStorage.getItem("BBB_fullname")).then((val)=>{
-            console.log(val)
-            if (!val.running&&val.visitor) {
-                getter("https://reservation.jitsi-mathu.de/deleteActiveMeeting?meetingID="+sessionStorage.getItem("BBB_meetingID"))
-                index= 1
-            }else if (val.running&&val.visitor){
-                index = 2
-            }else{
-                index=0
-            }
+        getter("/getSettingJson").then(async (settingJson)=>{
+            console.log(settingJson)
+            settings = await getter("https://reservation.jitsi-mathu.de/getKonfSettings")
+            getter("https://reservation.jitsi-mathu.de/getActiveMeetings?meetingID="+sessionStorage.getItem("BBB_meetingID")+"&name="+sessionStorage.getItem("BBB_fullname")).then((val)=>{
+                console.log(val)
+                if (!val.running&&val.visitor) {
+                    getter("https://reservation.jitsi-mathu.de/deleteActiveMeeting?meetingID="+sessionStorage.getItem("BBB_meetingID"))
+                    index= 1
+                }else if (val.running&&val.visitor){
+                    index = 2
+                }else{
+                    index=0
+                }
 
-            if(localStorage.getItem("settingsIndex")===null){
-                localStorage.setItem("settingsIndex",index.toString())
-                localStorage.setItem("indexTimer",new Date().toLocaleTimeString())
-            }else{
-                index = parseInt(localStorage.getItem("settingsIndex"))
-            }
-            console.log(index)
-            console.log(val)
-            getContent().then((content) => {
-                currentSettings = settings[index].value
-                console.log(settings)
-                changeButtonSize(currentSettings[0], content)
-                settingsNavBar(currentSettings[1], content)
-                disableChat(currentSettings[2],content)
-                disableUserList(currentSettings[3], content)
-                togglePresentation(currentSettings[4], content)
+                if(localStorage.getItem("settingsIndex")===null){
+                    localStorage.setItem("settingsIndex",index.toString())
+                    localStorage.setItem("indexTimer",new Date().toLocaleTimeString())
+                }else{
+                    index = parseInt(localStorage.getItem("settingsIndex"))
+                }
+                console.log(index)
+                console.log(val)
+                getContent().then((content) => {
+                    currentSettings = settings[index].value
+                    console.log(settings)
+                    changeButtonSize(currentSettings[0], content)
+                    settingsNavBar(currentSettings[1], content)
+                    disableChat(currentSettings[2],content)
+                    disableUserList(currentSettings[3], content)
+                    togglePresentation(currentSettings[4], content)
+                })
             })
-        })
 
+        })
     }, 2000);
 }, false)
 
